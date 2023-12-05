90a2fe6add35a1d99962d306bd58a105028c272a
Nguyễn Nhật Linh <72988946+nhatlinhis@users.noreply.github.com>
2023-12-05 09:21:22 +0700

    213

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,14 +1,48 @@
 import React from "react";
+import MovieList from "../components/movies/MovieList";
+import { fetcher } from "../config";
+import useSWR from "swr";
+import MovieCard from "../components/movies/MovieCard";
 
 const MoviePage = () => {
+  const { data, error, isLoading } = useSWR(
+    `https://api.themoviedb.org/3/movie/popular?api_key=305e809e770e39392a1a79bc909fdfc4`,
+    fetcher
+  );
+  const movies = data?.results || [];
   return (
-    <div>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry's standard dummy text ever since the
-      1500s, when an unknown printer took a galley of type and scrambled it to
-      make a type specimen book. It has survived not only five centuries, but
-      also the leap into electronic typesetting, remaining essentially
-      unchanged.
+    <div className="py-10 page-container">
+      <div className="flex mb-10">
+        <div className="flex-1">
+          <input
+            type="text"
+            className="w-full p-4 bg-transparent bg-slate-800  outline-none"
+            placeholder="Type  here to search..."
+          />
+        </div>
+        <button className="p-3 bg-primary text-white ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="grid grid-cols-4 gap-10">
+        {movies.length > 0 &&
+          movies.map((item) => (
+            <MovieCard key={item.id} item={item}></MovieCard>
+          ))}
+      </div>
     </div>
   );
 };
